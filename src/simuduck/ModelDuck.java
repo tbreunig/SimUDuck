c35d3a3caca95a4ffe2dc4a624b10357187213a9
Tyler <Tyler@10.30.17.198>
2013-09-15 15:42:45 -0500

    Kind of getting it. Still have some questions about my Duck class.

---
+++ src/simuduck/ModelDuck.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuduck;
+
+/**
+ *
+ * @author Tyler
+ */
+public class ModelDuck extends Duck {
+    public ModelDuck(){
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+    
+    @Override
+    public void display(){
+        System.out.println("I'm a model duck!");
+    }
+}
--- src/simuduck/SimUDuck.java
+++ src/simuduck/SimUDuck.java
@@ -7,8 +7,14 @@ package simuduck;
 public class SimUDuck {
 
     public static void main(String[] args) {
+    
      Duck mallard = new MallardDuck();
      mallard.performQuack();
      mallard.performFly();
-    }
+    
+    Duck model = new ModelDuck();
+    model.performFly();
+    model.setFlyBehavior(new FlyRocketPowered());
+    model.performFly();
+    }   
 }
